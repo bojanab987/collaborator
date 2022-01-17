e7f6c14e1742c9b44f93beb536fd7bc30a009662
Bojana Buljic <bojana.buljic@gmail.com>
2022-01-17 16:07:43 +0100

    [spliting] Add question string and btn

--- src/pages/ShowOff/index.tsx
+++ src/pages/ShowOff/index.tsx
@@ -5,22 +5,44 @@ import { useInterval } from './useInterval';
 const ShowOff = () => {
     const [message, setMessage] = useState('');
     const [isYellow, setIsYellow] = useState(true);
+    const [count, setCount] = useState(0);
+    const [question, setQuestion] = useState('');
+    const [isBtnVisible, setBtnVisible] = useState(false);
 
-    const printMessage = () => {
-        setMessage('I am Async');
+    const printMessage = (msg: string) => {
+        setMessage(msg);
     };
 
     const changeColor = () => {
         setIsYellow((isYellow) => !isYellow);
     };
 
+    const askQuestion = (str: string) => {
+        setQuestion(str);
+        setBtnVisible(true);
+    };
+
+    const handleClick = () => {
+        setBtnVisible(false);
+        setQuestion('');
+    };
+
     useEffect(() => {
-        const timer = setTimeout(() => printMessage(), 5000);
+        const timer = setTimeout(() => printMessage('I am Async'), 5000);
         return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const timer2 = setTimeout(
+            () => askQuestion('Are you asleep?'),
+            1000 * 60
+        );
+        return () => clearTimeout(timer2);
     }, []);
 
     useInterval(() => {
         changeColor();
+        setCount((count) => count + 1);
     }, 1000);
 
     return (
@@ -32,6 +54,20 @@ const ShowOff = () => {
             }
         >
             {message}
+            <p className={styles.white}>{count}</p>
+            <div className={styles.question}>
+                {question}
+                <button
+                    className={
+                        isBtnVisible
+                            ? `${styles.btn} ${styles.visible}`
+                            : ` ${styles.btn} ${styles.hidden}`
+                    }
+                    onClick={handleClick}
+                >
+                    NO
+                </button>
+            </div>
         </div>
     );
 };
